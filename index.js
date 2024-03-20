eecac264749ea3032e179a5d4cee520f42cd6827
Karmvir <jadejakarmvir12@gmail.com>
2024-03-20 22:54:30 +0530

    Here created a route for the user hover and connected the ejs file for dynamic showcase

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 // HINTS:
 // 1. Import express and axios
+import axios from "axios";
+import express from "express";
 
-// 2. Create an express app and set the port number.
+const app =express();
+const port =3000;
+const API_URL="https://secrets-api.appbrewery.com";
+
+
+app.use(express.static("public"));
+   
+app.get("/",async(req,res)=>{
+try{
+const result=await axios.get(API_URL);
+res.render("index.ejs",{
+    secret:result.data.secret,
+    user:result.data.user,
+    
+});
+} catch(error){
+    console.log(error.response.data);
+    res.result(500);
+}
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
 
 // 3. Use the public folder for static files.
 
-// 4. When the user goes to the home page it should render the index.ejs file.
+// 4. When the user goes to the home page it should render the index.ejs file
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
